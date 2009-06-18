aaa80fb6d283d890c36acfe2be18758a91f96f78
Marcin Kulik <marcin.kulik@gmail.com>
2009-06-18 23:45:27 +0200

    updated spinner, set min dialog size

---
+++ common-src/net/sickill/off/common/StatusLabel.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package net.sickill.off.common;
+
+import java.awt.Dimension;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+class StatusLabel extends JLabel {
+    private Icon spinnerIcon = new ImageIcon(OffPanel.class.getResource("spinner.gif"));
+
+    public StatusLabel() {
+        super(" ");
+    }
+
+    public Dimension getPreferredSize() {
+        return new Dimension(super.getPreferredSize().width, 16);
+    }
+
+    public void setIndexing(boolean b) {
+        if (b) {
+            setText("Indexing project files, please wait...");
+            setIcon(spinnerIcon);
+        } else {
+            setText(" ");
+            setIcon(null);
+        }
+    }
+}
+
