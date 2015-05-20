fadfa9fc806485b9da8dc078b4e40a0a5a9f916a
Andres Kalle <mjomble@gmail.com>
2015-05-20 16:30:45 +0300

    Code cleanup

--- netbeans/src/net/sickill/off/common/OffPanel.java
+++ netbeans/src/net/sickill/off/common/OffPanel.java
@@ -5,7 +5,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.io.IOException;
 import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
